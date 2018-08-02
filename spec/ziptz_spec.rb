dbb0671f163aa557f0752e9714bf5c193473bd61
Keith Morrison <keithm@infused.org>
2018-08-02 12:11:40 -0700

    10x faster specs

--- spec/ziptz_spec.rb
+++ spec/ziptz_spec.rb
@@ -1,71 +1,63 @@
 require 'spec_helper'
 
 describe Ziptz do
-  describe 'when inspected' do
-    let(:ziptz) { Ziptz.new }
+  before(:all) { @ziptz = Ziptz.new }
 
+  describe 'when inspected' do
     it 'does not show internal instance variables' do
-      expect(ziptz.inspect).to match /#<Ziptz:\d+>/
+      expect(@ziptz.inspect).to match(/#<Ziptz:\d+>/)
     end
   end
 
   describe '#time_zone_name' do
-    let(:ziptz) { Ziptz.new }
-
     context 'when given a 5-digit zipcode' do
       it 'returns the time zone number' do
-        expect(ziptz.time_zone_name('97034')).to eq 'America/Los_Angeles'
+        expect(@ziptz.time_zone_name('97034')).to eq 'America/Los_Angeles'
       end
     end
 
     context 'when there is no matching zipcode' do
       it 'returns nil' do
-        expect(ziptz.time_zone_name('xyz')).to be_nil
+        expect(@ziptz.time_zone_name('xyz')).to be_nil
       end
     end
   end
 
   describe '#time_zone_uses_dst?' do
-    let(:ziptz) { Ziptz.new }
-
     context 'when given a 5-digit zipcode' do
       it 'returns the time zone number' do
-        expect(ziptz.time_zone_name('97034')).to eq 'America/Los_Angeles'
+        expect(@ziptz.time_zone_name('97034')).to eq 'America/Los_Angeles'
       end
     end
   end
 
   describe '#time_zone_offset' do
-    let(:ziptz) { Ziptz.new }
-
     context 'when given a 5-digit zipcode' do
       it 'returns a boolean' do
-        expect(ziptz.time_zone_uses_dst?('97034')).to eq true
-        expect(ziptz.time_zone_uses_dst?('85004')).to eq false
+        expect(@ziptz.time_zone_uses_dst?('97034')).to eq true
+        expect(@ziptz.time_zone_uses_dst?('85004')).to eq false
       end
     end
 
     context 'when there is no matching zipcode' do
       it 'returns nil' do
-        expect(ziptz.time_zone_offset('xyz')).to be_nil
+        expect(@ziptz.time_zone_offset('xyz')).to be_nil
       end
     end
   end
 
   describe '#zips' do
     context 'when given a time zone' do
-      let(:ziptz) { Ziptz.new }
-
       it 'returns an array of zip codes' do
-        expect(ziptz.zips('Pacific/Pago_Pago')).to eq %w(96799)
+        expect(@ziptz.zips('Pacific/Pago_Pago')).to eq %w[96799]
       end
 
       it 'is case-insensitive' do
-        expect(ziptz.zips('Pacific/Pago_Pago')).to eq %w(96799)
+        expect(@ziptz.zips('Pacific/Pago_Pago')).to eq %w[96799]
       end
 
       it 'returns nil for unknown time zones' do
-        expect(ziptz.zips('Glark')).to be_nil
+        expect(@ziptz.zips('Glark')).to be_nil
       end
     end
   end
