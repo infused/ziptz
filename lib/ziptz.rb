4e3c43bab2524afa44f8d18bb3ceb9e797620854
Keith Morrison <keithm@infused.org>
2021-02-22 16:24:44 -0800

    restructure data storage

--- lib/ziptz.rb
+++ lib/ziptz.rb
@@ -1,40 +1,33 @@
 require 'yaml'
+require 'zlib'
 
 class Ziptz
   VERSION = '2.1.14'.freeze
 
-  TZ_INFO = [
-    {name: 'America/Puerto_Rico', offset: -4},
-    {name: 'America/New_York', offset: -5},
-    {name: 'America/Chicago', offset: -6},
-    {name: 'America/Denver', offset: -7},
-    {name: 'America/Los_Angeles', offset: -8},
-    {name: 'America/Anchorage', offset: -9},
-    {name: 'Pacific/Honolulu', offset: -10},
-    {name: 'Pacific/Pago_Pago', offset: -11},
-    {name: 'Pacific/Majuro', offset: 12},
-    {name: 'Pacific/Guam', offset: 10},
-    {name: 'Pacific/Palau', offset: 9},
-    {name: 'Pacific/Pohnpei', offset: 11},
-    {name: 'America/Phoenix', offset: -7},
-    {name: 'America/Adak', offset: -10}
-  ].freeze
-
   def time_zone_name(zip)
-    get_time_zone(zip)
+    tz_info = time_zone_info(zip)
+    return unless tz_info
+
+    tz_info[:tz]
   end
 
   def time_zone_offset(zip)
-    tz = time_zone_info(zip)
-    tz && tz[:offset]
+    tz_info = time_zone_info(zip)
+    return unless tz_info
+
+    tzm_info = tzm[tz_info[:tz]]
+    tzm_info[:offset] && tzm_info[:offset].to_i / 60 / 60
   end
 
   def time_zone_uses_dst?(zip)
-    dst[zip.to_s.slice(0, 5)]
+    tz_info = time_zone_info(zip)
+    return unless tz_info
+
+    tz_info[:dst]
   end
 
   def zips(tz_name)
-    zips_by_code(tz_name)
+    tz.select { |_, v| v[:tz].downcase == tz_name.downcase }.keys
   end
 
   def inspect
@@ -43,24 +36,20 @@ class Ziptz
 
   protected
 
+  def tzm
+    @tzm ||= load_tzm_data
+  end
+
   def tz
     @tz ||= load_tz_data
   end
 
-  def dst
-    @dst ||= load_dst_data
-  end
-
-  def zips_by_code(tz_code)
-    tz.select { |_, v| v.match(/#{tz_code}/i) }.keys.sort
-  end
-
   def time_zone_info(zip)
-    TZ_INFO.detect { |v| v[:name] == get_time_zone(zip) }
+    tz[zip.to_s.slice(0, 5)]
   end
 
-  def get_time_zone(zip)
-    tz[zip.to_s.slice(0, 5)]
+  def tzm_data_path
+    File.join(File.dirname(__FILE__), '..', 'data', 'tzm.data')
   end
 
   def tz_data_path
@@ -69,19 +58,15 @@ class Ziptz
 
   def load_tz_data
     File.foreach(tz_data_path).with_object({}) do |line, data|
-      zip, tz = line.strip.split('=')
-      data[zip] = tz
+      zip, tz, dst = line.strip.split(':')
+      data[zip] = {tz: tz, dst: dst == '1'}
     end
   end
 
-  def dst_data_path
-    File.join(File.dirname(__FILE__), '..', 'data', 'dst.data')
-  end
-
-  def load_dst_data
-    File.foreach(dst_data_path).with_object({}) do |line, data|
-      zip, dst = line.strip.split('=')
-      data[zip] = dst == '1'
+  def load_tzm_data
+    File.foreach(tzm_data_path).with_object({}) do |line, data|
+      tz, offset = line.strip.split(':')
+      data[tz] = {offset: offset}
     end
   end
 end
